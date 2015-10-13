33cae15f64ecb2c4a1fa973e2322d8f0a938662e
Paul Crozier <pcrozier@knights.ucf.edu>
2015-10-13 16:15:45 -0400

    Fixed the formatting, but nothing else.

--- marrh.c
+++ marrh.c
@@ -4,171 +4,183 @@
 #define  PICSIZE 256
 #define  MAXMASK 100
 
-         int    pic[PICSIZE][PICSIZE];
-         double outpic1[PICSIZE][PICSIZE];
-         double outpic2[PICSIZE][PICSIZE];
-         int    edgeflag[PICSIZE][PICSIZE];
-         double mask[MAXMASK][MAXMASK];
-         double conv[PICSIZE][PICSIZE];
+int    pic[PICSIZE][PICSIZE];
+double outpic1[PICSIZE][PICSIZE];
+double outpic2[PICSIZE][PICSIZE];
+int    edgeflag[PICSIZE][PICSIZE];
+double mask[MAXMASK][MAXMASK];
+double conv[PICSIZE][PICSIZE];
 
 main(argc,argv)
 int argc;
 char **argv;
 {
-        int     i,j,p,q,s,t,mr,centx,centy;
-        double  maskval,sum,sig,maxival,minival,maxval,ZEROTOL,percentage;
-        FILE    *fo1, *fo2,*fp1, *fopen();
-        char    *foobar;
+    int     i,j,p,q,s,t,mr,centx,centy;
+    double  maskval,sum,sig,maxival,minival,maxval,ZEROTOL,percentage;
+    FILE    *fo1, *fo2,*fp1, *fopen();
+    char    *foobar;
 
-        // Get input fname
-        argc--; argv++;
-        foobar = *argv;
-        fp1=fopen(foobar,"rb");
+    // Get input fname
+    argc--;
+    argv++;
+    foobar = *argv;
+    fp1=fopen(foobar,"rb");
 
-        // Get output fname
-        argc--; argv++;
-        foobar = *argv;
-        fo1=fopen(foobar,"wb");
+    // Get output fname
+    argc--;
+    argv++;
+    foobar = *argv;
+    fo1=fopen(foobar,"wb");
 
-        //argc--; argv++;
-        //foobar = *argv;
-        //fo2=fopen(foobar,"wb");
+    //argc--; argv++;
+    //foobar = *argv;
+    //fo2=fopen(foobar,"wb");
 
-        // Get sigma
-        argc--; argv++;
-        foobar = *argv;
-        sig = atof(foobar);
-        //sig = (double)atoi(foobar);
-        printf("\n%s %.2lf\n",foobar,sig);
-        // Get percentage
-        argc--; argv++;
-        foobar = *argv;
-        //percentage = atof(foobar);
-        ZEROTOL = atof(foobar);
+    // Get sigma
+    argc--;
+    argv++;
+    foobar = *argv;
+    sig = atof(foobar);
+    //sig = (double)atoi(foobar);
+    printf("\n%s %.2lf\n",foobar,sig);
+    // Get percentage
+    argc--;
+    argv++;
+    foobar = *argv;
+    //percentage = atof(foobar);
+    ZEROTOL = atof(foobar);
 
-        mr = (int)(sig * 3);
-        centx = (MAXMASK / 2);
-        centy = (MAXMASK / 2);
-        printf("1 ");
-        for (i=0;i<256;i++)
-        { for (j=0;j<256;j++)
+    mr = (int)(sig * 3);
+    centx = (MAXMASK / 2);
+    centy = (MAXMASK / 2);
+    printf("1 ");
+    for (i=0; i<256; i++)
+    {
+        for (j=0; j<256; j++)
+        {
+            pic[i][j]  =  getc (fp1);
+        }
+    }
+    printf("1-2 ");
+    for (p=-mr; p<=mr; p++)
+    {
+        for (q=-mr; q<=mr; q++)
+        {
+            printf("1-3 ");
+            maskval = ((2-(((p*p)+(q*q))/(sig*sig)))*
+                       (exp(-1*(((p*p)+(q*q))/(2*(sig*sig))))));
+            printf("1-4 ");
+            printf("\n %.2lf %d %d %d %d %d %d\n ",sig,mr,p,q,centx,centy,maskval);
+            (mask[p+centy][q+centx]) = maskval;
+            printf("1-5 ");
+        }
+    }
+    printf("2 ");
+    for (i=mr; i<=255-mr; i++)
+    {
+        for (j=mr; j<=255-mr; j++)
+        {
+            sum = 0;
+            for (p=-mr; p<=mr; p++)
+            {
+                for (q=-mr; q<=mr; q++)
                 {
-                  pic[i][j]  =  getc (fp1);
+                    sum += pic[i+p][j+q] * mask[p+centy][q+centx];
                 }
+            }
+            outpic1[i][j] = sum;
+            conv[i][j] = sum;
         }
-        printf("1-2 ");
-        for (p=-mr;p<=mr;p++)
-        {  for (q=-mr;q<=mr;q++)
-           {
-               printf("1-3 ");
-              maskval = ((2-(((p*p)+(q*q))/(sig*sig)))*
-                      (exp(-1*(((p*p)+(q*q))/(2*(sig*sig))))));
-                      printf("1-4 ");
-                      printf("\n %.2lf %d %d %d %d %d %d\n ",sig,mr,p,q,centx,centy,maskval);
-              (mask[p+centy][q+centx]) = maskval;
-              printf("1-5 ");
-           }
-        }
-        printf("2 ");
-        for (i=mr;i<=255-mr;i++)
-        { for (j=mr;j<=255-mr;j++)
-          {
-             sum = 0;
-             for (p=-mr;p<=mr;p++)
-             {
-                for (q=-mr;q<=mr;q++)
-                {
-                   sum += pic[i+p][j+q] * mask[p+centy][q+centx];
-                }
-             }
-             outpic1[i][j] = sum;
-             conv[i][j] = sum;
-          }
-        }
-        printf("3 ");
-        maxval  = 0;
-        maxival = 0;
-        minival = 255;
+    }
+    printf("3 ");
+    maxval  = 0;
+    maxival = 0;
+    minival = 255;
 
-        for (i=mr;i<256-mr;i++)
-        { for (j=mr;j<256-mr;j++)
-          {
-             if (outpic1[i][j] > maxival)
+    for (i=mr; i<256-mr; i++)
+    {
+        for (j=mr; j<256-mr; j++)
+        {
+            if (outpic1[i][j] > maxival)
                 maxival = outpic1[i][j];
-             if (outpic1[i][j] < minival)
+            if (outpic1[i][j] < minival)
                 minival = outpic1[i][j];
-           }
         }
-        if (fabs(maxival) > fabs(minival))
-           maxval = fabs(maxival);
-        else
-           maxval = fabs(minival);
-        printf("4 ");
-        for (i=0;i<256;i++)
-        { for (j=0;j<256;j++)
-          {
-             outpic1[i][j] = ((((outpic1[i][j]) / maxval) + 1) * 127);
-             fprintf(fo1,"%c",(char)((int)(outpic1[i][j])));
-          }
+    }
+    if (fabs(maxival) > fabs(minival))
+        maxval = fabs(maxival);
+    else
+        maxval = fabs(minival);
+    printf("4 ");
+    for (i=0; i<256; i++)
+    {
+        for (j=0; j<256; j++)
+        {
+            outpic1[i][j] = ((((outpic1[i][j]) / maxval) + 1) * 127);
+            fprintf(fo1,"%c",(char)((int)(outpic1[i][j])));
         }
-        printf("5 ");
-        for (i=mr;i<=255-mr;i++)
-        {  for (j=mr;j<=255-mr;j++)
-           {
-                         outpic2[i][j] = 0;
-             if (conv[i][j] > ZEROTOL)
-             {
-               for (p=-1;p<=1;p++)
-               {
-                 for (q=-1;q<=1;q++)
-                 {
-                   if (conv[i+p][j+q] < -ZEROTOL)
-                   {
-                     outpic2[i][j] = 255;
-                   }
-                 }
-               }
-             }
-             else if ((fabs)(conv[i][j]) < ZEROTOL)
-             {
-                     if (((conv[i+1][j] > ZEROTOL) &&
-                          (conv[i-1][j] < -ZEROTOL))   ||
-                         ((conv[i+1][j] < -ZEROTOL) &&
-                          (conv[i-1][j] > ZEROTOL)))
-                     {
-                       outpic2[i][j] = 255;
-                     }
-                     else if (((conv[i][j+1] > ZEROTOL) &&
-                               (conv[i][j-1] < -ZEROTOL))   ||
-                              ((conv[i][j+1] < -ZEROTOL) &&
-                               (conv[i][j-1] > ZEROTOL)))
-                     {
-                       outpic2[i][j] = 255;
-                     }
-                     else if (((conv[i+1][j+1] > ZEROTOL) &&
-                               (conv[i-1][j-1] < -ZEROTOL))   ||
-                              ((conv[i+1][j+1] < -ZEROTOL) &&
-                               (conv[i-1][j-1] > ZEROTOL)))
-                     {
-                       outpic2[i][j] = 255;
-                     }
-                     else if (((conv[i+1][j-1] > ZEROTOL) &&
-                               (conv[i-1][j+1] < -ZEROTOL))   ||
-                              ((conv[i+1][j-1] < -ZEROTOL) &&
-                               (conv[i-1][j+1] > ZEROTOL)))
-                     {
-                       outpic2[i][j] = 255;
-                     }
-             }
-           }
+    }
+    printf("5 ");
+    for (i=mr; i<=255-mr; i++)
+    {
+        for (j=mr; j<=255-mr; j++)
+        {
+            outpic2[i][j] = 0;
+            if (conv[i][j] > ZEROTOL)
+            {
+                for (p=-1; p<=1; p++)
+                {
+                    for (q=-1; q<=1; q++)
+                    {
+                        if (conv[i+p][j+q] < -ZEROTOL)
+                        {
+                            outpic2[i][j] = 255;
+                        }
+                    }
+                }
+            }
+            else if ((fabs)(conv[i][j]) < ZEROTOL)
+            {
+                if (((conv[i+1][j] > ZEROTOL) &&
+                        (conv[i-1][j] < -ZEROTOL))   ||
+                        ((conv[i+1][j] < -ZEROTOL) &&
+                         (conv[i-1][j] > ZEROTOL)))
+                {
+                    outpic2[i][j] = 255;
+                }
+                else if (((conv[i][j+1] > ZEROTOL) &&
+                          (conv[i][j-1] < -ZEROTOL))   ||
+                         ((conv[i][j+1] < -ZEROTOL) &&
+                          (conv[i][j-1] > ZEROTOL)))
+                {
+                    outpic2[i][j] = 255;
+                }
+                else if (((conv[i+1][j+1] > ZEROTOL) &&
+                          (conv[i-1][j-1] < -ZEROTOL))   ||
+                         ((conv[i+1][j+1] < -ZEROTOL) &&
+                          (conv[i-1][j-1] > ZEROTOL)))
+                {
+                    outpic2[i][j] = 255;
+                }
+                else if (((conv[i+1][j-1] > ZEROTOL) &&
+                          (conv[i-1][j+1] < -ZEROTOL))   ||
+                         ((conv[i+1][j-1] < -ZEROTOL) &&
+                          (conv[i-1][j+1] > ZEROTOL)))
+                {
+                    outpic2[i][j] = 255;
+                }
+            }
         }
+    }
 
-        for (i=0;i<256;i++)
-        { for (j=0;j<256;j++)
-          {  if (outpic2[i][j] == 255) outpic2[i][j]=0;
-             else outpic2[i][j]=255;
-             fprintf(fo2,"%c",(char)((int)(outpic2[i][j])));
-          }
+    for (i=0; i<256; i++)
+    {
+        for (j=0; j<256; j++)
+        {
+            if (outpic2[i][j] == 255) outpic2[i][j]=0;
+            else outpic2[i][j]=255;
+            fprintf(fo2,"%c",(char)((int)(outpic2[i][j])));
         }
+    }
 }
 
